aa16b98d5efb4c1733133424057e9b61d0f9f83a
c00k1ez <egorplotnikov@mail.ru>
2021-03-19 20:16:30 +0300

    init commit

---
+++ plain_transformers/common_layers.py
@@ -0,0 +1,137 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class FFN(nn.Module):
+    def __init__(
+            self,
+            d_model=512,
+            dim_feedforward=2048,
+            dropout=0.1
+        ):
+        super(FFN, self).__init__()   
+        self.d_model = d_model
+        self.dim_feedforward = dim_feedforward
+        self.layer_inc = nn.Linear(d_model, dim_feedforward)
+        self.layer_reduce = nn.Linear(dim_feedforward, d_model)
+        self.dropout = nn.Dropout(dropout)
+   
+    def forward(self, hidden):
+        hid_state = self.layer_inc(hidden)
+        hid_state = self.dropout(F.relu(hid_state))
+        return self.layer_reduce(hid_state)
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(
+            self,
+            d_model=512,
+            n_heads=8,
+            dropout=0.1,
+            query_input_dim=None,
+            key_input_dim=None,
+            value_input_dim=None
+        ):
+        super(MultiHeadAttention, self).__init__()
+        assert d_model % n_heads == 0
+        self.d_model = d_model
+        self.n_heads = n_heads
+        self.hidden_per_head = d_model // n_heads
+        self.scale = self.hidden_per_head ** 0.5
+        self.query_input_dim = d_model if query_input_dim is None else query_input_dim
+        self.key_input_dim = d_model if key_input_dim is None else key_input_dim
+        self.value_input_dim = d_model if value_input_dim is None else value_input_dim
+
+        self.key_projection = nn.Linear(self.key_input_dim, d_model)
+        self.query_projection = nn.Linear(self.query_input_dim, d_model)
+        self.value_projection = nn.Linear(self.value_input_dim, d_model)
+        self.dropout = nn.Dropout(dropout)
+
+    def transpose_to_heads(self, x):
+        # (batch_size, seq_len, emb_dim) -> (batch_size, seq_len, n_heads, hidden_per_head)
+        new_shape = x.size()[:-1] + (self.n_heads, self.hidden_per_head)
+        x = x.view(*new_shape)
+        # (batch_size, seq_len, n_heads, hidden_per_head) ->
+        # -> (batch_size, n_heads, seq_len, hidden_per_head)
+        return x.permute(0, 2, 1, 3)
+
+    def forward(
+            self,
+            query,
+            key,
+            value,
+            attention_mask=None,
+            use_decoder_mask=False,
+            get_attention_scores=False
+        ):
+        query_proj = self.query_projection(query)
+        key_proj = self.key_projection(key)
+        value_proj = self.value_projection(value)
+
+        query_proj = self.transpose_for_scores(query_proj)
+        key_proj = self.transpose_for_scores(key_proj)
+        value_proj = self.transpose_for_scores(value_proj)
+
+        raw_scores = torch.matmul(query_proj, key_proj.transpose(-1, -2))
+
+        if attention_mask is not None:
+            raw_scores = raw_scores + attention_mask       
+        attn_scores = F.softmax(raw_scores / self.scale, dim=-1)
+        attn = torch.matmul(attn_scores, value_proj)
+        attn = attn.permute(0, 2, 1, 3).contiguous()
+        new_shape = attn.size()[:-2] + (self.d_model,)
+        attn = attn.view(*new_shape)
+        output = (attn, )
+        if get_attention_scores:
+            output = output + (attn_scores, )
+        return output
+
+
+class TransformerEmbedding(nn.Module):
+    def __init__(
+            self,
+            vocab_size,
+            d_model,
+            max_length,
+            pad_token_id,
+            token_type_vocab_size,
+            pos_embedding_type='embedding',
+            dropout=0.1,
+            use_layer_norm=False
+        ):
+        super(TransformerEmbedding, self).__init__()
+        assert pos_embedding_type in ['embedding', 'timing']
+        # TODO: implement timing signal
+        self.token_embedding = nn.Embedding(vocab_size, d_model, pad_token_id)
+        self.positional_embedding = nn.Embedding(max_length, d_model)
+        self.token_type_embedding = nn.Embedding(token_type_vocab_size, d_model)
+
+        self.dropout = nn.Dropout(dropout)
+        self.register_buffer("pos_ids", torch.arange(max_length).expand((1, -1)))
+
+        self.layer_norm = nn.LayerNorm(d_model)
+
+        self.use_layer_norm = use_layer_norm
+
+    def forward(
+            self,
+            input_ids,
+            token_type_ids=None
+        ):
+        input_shape = input_ids.shape
+        token_emb = self.token_embedding(input_ids)
+        if token_type_ids is not None:
+            token_emb = token_emb + self.token_type_embedding(token_type_ids)
+        else:
+            token_emb = token_emb + torch.zeros(input_shape, dtype=torch.long, device=self.pos_ids.device)
+        
+        pos_ids = self.pos_ids[:, :input_shape[1]]
+
+        token_emb = token_emb + self.positional_embedding(pos_ids)
+
+        if self.use_layer_norm:
+            token_emb = self.layer_norm(token_emb)
+        
+        token_emb = self.dropout(token_emb)
+        return token_emb
---
+++ plain_transformers/pre_ln_encoder.py
@@ -0,0 +1,154 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from common_layers import FFN, MultiHeadAttention, TransformerEmbedding
+
+
+class PreLNEncoderLayer(nn.Module):
+    def __init__(
+            self,
+            d_model=512,
+            n_heads=8,
+            dim_feedforward=2048,
+            dropout=0.1
+        ):
+        super(PreLNEncoderLayer, self).__init__()
+        self.self_attention = MultiHeadAttention(d_model, n_heads, dropout)
+        self.dropout = nn.Dropout(dropout)
+        self.ffn = FFN(d_model, dim_feedforward, dropout)
+        self.pre_attn_ln = nn.LayerNorm(d_model)
+        self.pre_ffn_ln = nn.LayerNorm(d_model)
+
+    def forward(
+            self,
+            hidden,
+            attention_mask=None,
+            get_attention_scores=False
+        ):
+        attn_scores = None
+        block_state = self.pre_attn_ln(hidden)
+        block_state = self.self_attention(
+            query=block_state,
+            key=block_state,
+            value=block_state,
+            attention_mask=attention_mask,
+            get_attention_scores=get_attention_scores
+        )
+        if get_attention_scores:
+            attn_scores = block_state[1]
+        block_state = block_state[0]
+        block_state = block_state + hidden
+
+        ffn_block_state = self.pre_ffn_ln(block_state)
+        ffn_block_state = self.ffn(ffn_block_state)
+        ffn_block_state = ffn_block_state + block_state
+
+        output = (ffn_block_state, )
+        if get_attention_scores:
+            output = output + (attn_scores, )
+        return output
+
+
+class PreLNEncoder(nn.Module):
+    def __init__(
+            self,
+            num_layers,
+            **kwargs
+        ):
+        super(PreLNEncoder, self).__init__()
+        self.encoder_layers = nn.ModuleList([
+            PreLNEncoderLayer(**kwargs) for _ in range(num_layers)
+        ])
+
+    def forward(
+            self,
+            hidden,
+            attention_mask=None,
+            get_attention_scores=False
+        ):
+        attn_scores = []
+        for layer in self.encoder_layers:
+            hidden = layer(
+                hidden,
+                attention_mask=attention_mask,
+                get_attention_scores=get_attention_scores
+            )
+            if get_attention_scores:
+                attn_scores.append(hidden[1])
+            hidden = hidden[0]
+        output = (hidden, )
+        if get_attention_scores:
+            attn_scores = torch.stack(attn_scores, dim=-1)
+            output = output + (attn_scores, )
+        return output
+
+
+class PreLNTransformerEncoder(nn.Module):
+    def __init__(
+            self,
+            d_model,
+            vocab_size,
+            max_length,
+            pad_token_id,
+            token_type_vocab_size,
+            dropout,
+            n_heads,
+            dim_feedforward,
+            num_layers,
+            use_layer_norm=False,
+            pos_embedding_type='embedding'
+        ):
+        super(PreLNTransformerEncoder, self).__init__()
+        self.embedding = TransformerEmbedding(
+            vocab_size=vocab_size,
+            d_model=d_model,
+            max_length=max_length,
+            pad_token_id=pad_token_id,
+            token_type_vocab_size=token_type_vocab_size,
+            pos_embedding_type=pos_embedding_type,
+            dropout=dropout,
+            use_layer_norm=use_layer_norm
+        )
+        self.encoder = PreLNEncoder(
+            num_layers=num_layers,
+            d_model=d_model,
+            n_heads=n_heads,
+            dim_feedforward=dim_feedforward,
+            dropout=dropout
+        )
+
+        self.post_encoder_ln = nn.LayerNorm(d_model)
+
+    def create_attention_mask(self, attention_mask, input_shape, device):
+        # [batch_size, seq_len] -> [batch_size, 1, 1, seq_len]
+        if attention_mask is None:
+            attention_mask = torch.ones(*input_shape, device=device)
+        attention_mask = attention_mask.unsqueeze(1).unsqueeze(1)
+        attention_mask = (1.0 - attention_mask) * -10000.0
+        return attention_mask
+
+    def forward(
+            self,
+            input_ids,
+            attention_mask=None,
+            token_type_ids=None,
+            get_attention_scores=False
+        ):
+        if attention_mask is None:
+            attention_mask = self.create_attention_mask(
+                attention_mask,
+                input_ids.shape[:1],
+                input_ids.device
+            )
+        embeddings = self.embedding(input_ids, token_type_ids)
+        hidden = self.encoder(
+            embeddings,
+            attention_mask=attention_mask,
+            get_attention_scores=get_attention_scores
+        )
+        hidden_ln = self.post_encoder_ln(hidden[0])
+        output = (hidden_ln, )
+        if get_attention_scores:
+            output = output + (hidden[1], )
+        return output
