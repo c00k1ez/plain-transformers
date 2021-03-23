8ec4f424eb3f135954218f3512aaf35bb1bb9e4b
c00k1ez <egorplotnikov@mail.ru>
2021-03-23 17:46:17 +0300

    fix

--- setup.py
+++ setup.py
@@ -6,17 +6,15 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 
 def _load_requirements(
-        path_dir: str,
-        file_name: str = 'requirements.txt',
-        comment_char: str = '#'
+    path_dir: str, file_name: str = "requirements.txt", comment_char: str = "#"
 ):
-    with open(os.path.join(path_dir, file_name), 'r') as file:
+    with open(os.path.join(path_dir, file_name), "r") as file:
         lines = [ln.strip() for ln in file.readlines()]
     reqs = []
     for ln in lines:
         if comment_char in ln:
-            ln = ln[:ln.index(comment_char)].strip()
-        if ln.startswith('http'):
+            ln = ln[: ln.index(comment_char)].strip()
+        if ln.startswith("http"):
             continue
         if ln:
             reqs.append(ln)
@@ -41,7 +39,9 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src", exclude=['tests/', 'tests/*']),
+    packages=setuptools.find_packages(
+        where="src", exclude=["tests/", "tests/*"]
+    ),
     python_requires=">=3.6",
-    install_requires=_load_requirements(os.path.dirname(__file__))
+    install_requires=_load_requirements(os.path.dirname(__file__)),
 )
