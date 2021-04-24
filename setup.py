ad057552a523c1d1dfd2fb2185c07e687c1f28ae
c00k1ez <egorplotnikov@mail.ru>
2021-04-24 22:50:53 +0300

    fix

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ def _load_requirements(
 
 setuptools.setup(
     name="plain-transformers",
-    version="0.0.1",
+    version="0.0.1.1",
     author="c00k1ez",
     author_email="c00k1ez.th13f@gmail.com",
     description="one more transformers lib",
