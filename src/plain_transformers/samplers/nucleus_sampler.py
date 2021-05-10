e686d1d6242bef969ddd0bea055bf08cbd770fec
c00k1ez <egorplotnikov@mail.ru>
2021-05-10 19:46:50 +0300

    update setup{.cfg, .py}

--- src/plain_transformers/samplers/nucleus_sampler.py
+++ src/plain_transformers/samplers/nucleus_sampler.py
@@ -18,24 +18,36 @@ from .base_sampler import BaseSampler
 
 
 class NucleusSampler(BaseSampler):
+    """
+    Sampling based on
+    https://gist.github.com/thomwolf/1a5a29f6962089e871b94cbd09daf317
+    """
+
     def __init__(self, *args, **kwargs) -> None:
         super(NucleusSampler, self).__init__(*args, **kwargs)
 
     @torch.no_grad()
     def sample(self, logits: torch.Tensor, temperatype: float, **kwargs) -> torch.Tensor:
-        top_k, top_p = None, None
+        # logits shape: [1, vocab_size]
+        top_k, top_p = logits.shape[-1], 0.0
         if "top_k" in kwargs:
             top_k = kwargs["top_k"]
         if "top_p" in kwargs:
             top_p = kwargs["top_p"]
-        assert (top_k is None and top_p is not None) or (top_k is not None and top_p is None)
-        probs = torch.softmax(logits / temperatype, dim=-1)
-        top_vals_inds = None
-        if top_k is not None:
-            top_vals_inds = probs.topk(top_k)
-            ind = torch.multinomial(top_vals_inds.values, 1)
-            max_val = top_vals_inds.indices[0, ind]
-        else:
-            # TODO: implement tok_p sampling
-            pass
-        return max_val
+        logits = logits / temperatype
+        if top_k > 0:
+            inds_to_delete = logits < torch.topk(logits, top_k)[0][..., -1, None]
+            logits[inds_to_delete] = float("-inf")
+
+        if top_p > 0.0:
+            sorted_logits, sorted_indices = torch.sort(logits, descending=True)
+            cumulative_probs = torch.cumsum(torch.softmax(sorted_logits, dim=-1), dim=-1)
+            sorted_indices_to_remove = cumulative_probs >= top_p
+            sorted_indices_to_remove[..., 1:] = sorted_indices_to_remove[..., :-1].clone()
+            sorted_indices_to_remove[..., 0] = 0
+            indices_to_remove = torch.zeros_like(logits, dtype=torch.uint8).scatter_(
+                dim=-1, index=sorted_indices, src=sorted_indices_to_remove
+            )  # .to(logits.device)
+            logits[indices_to_remove] = float("-inf")
+
+        return torch.multinomial(torch.softmax(logits, dim=-1), 1)
